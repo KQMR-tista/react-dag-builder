6c4e785e83b958954c6270d13f6ba42af7c368b8
Ajith Keerikkattil <ajithkeerikkattil@ajiths-air.myfiosgateway.com>
2024-08-28 11:50:39 -0400

    first commit

--- src/flow/CustomActivityNode.tsx
+++ src/flow/CustomActivityNode.tsx
@@ -1,28 +1,38 @@
 import React, {useState} from 'react';
-import {Handle, Position, NodeProps} from "@xyflow/react";
-import {Button, IconButton, InputLabel, MenuItem, Select} from "@mui/material";
+import {Handle, Position, NodeProps, Node} from "@xyflow/react";
+import {IconButton, InputLabel, MenuItem, Select} from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
 
-type PropData = {
-    label: string;
-    activityType: string,
-}
-export type DropdownProps = NodeProps<CustomData> & {
-    onButtonClick: (id: string) => void
-}
+let activitiesList: string[] = ['Build Docker Container', 'Push image to ECR', 'Deploy container to ECS' ]
 
-function CustomActivityNode(props: NodeProps<PropData>) {
+    /* Type definition of the custom activity node */
+    export type customActivityNode = Node<
+    {
+        label: string;
+        activityType: string;
+        onDeleteNodeButtonClick: (nodeId: string) => void;
+    },
+    'activityNode'>;
+
+/**
+ * CustomActivityNode component.
+ * @param data -> The destructured data prop of this component. onDeleteNodeButtonClick prop triggers the delete function in Parent.
+ * @param id -> Node ID
+ * @constructor
+ */
+function CustomActivityNode({data, id}: NodeProps<customActivityNode>) {
     let [selection, setSelection] = useState('');
 
+    /* Set the selected activity. Adds the selection to data prop passed up to parent */
     const onselectionchange = (e: any) => {
         setSelection(e.target.value);
-        props.data.activityType = (e.target.value);
+        data.activityType = (e.target.value);
     }
 
     return (
         <>
             <div style={{backgroundColor:'darkgray', paddingLeft: '20px', paddingBottom: '20px', paddingRight: '5px'}}>
-                <InputLabel style={{fontSize: '12px'}}>{`${props?.data.label}`}</InputLabel>
+                <InputLabel style={{fontSize: '12px'}}>{`${data.label}`}</InputLabel>
                 <div style={{display: "flex", flexDirection:"row"}}>
                     <Select style={{padding: '0px', width: '250px', backgroundColor: 'white'}}
                             className="nodrag nopan"
@@ -31,15 +41,14 @@ function CustomActivityNode(props: NodeProps<PropData>) {
                             label="Age"
                             onChange={onselectionchange}
                     >
-                        <MenuItem style={{fontSize: '12px'}} value={'Build Docker Container'}>Build Docker Container</MenuItem>
-                        <MenuItem style={{fontSize: '12px'}} value={'Push image to ECR'}>Push image to ECR</MenuItem>
-                        <MenuItem style={{fontSize: '12px'}} value={'Deploy container to ECS'}>Deploy container to ECS</MenuItem>
+                        {activitiesList.map(activity => (<MenuItem style={{fontSize: '12px'}} key={activity} value={activity}>{activity}</MenuItem>))}
                     </Select>
-                    <IconButton onClick={() => props.data.onButtonClick(props.id)}>
+                    <IconButton onClick={() => data.onDeleteNodeButtonClick(id)}>
                         <DeleteIcon style={{fontSize: '20px'}} />
                     </IconButton>
                 </div>
 
+                {/*Position of the edge connectors*/}
                 <Handle type="source" position={Position.Right}/>
                 <Handle type="target" position={Position.Left}/>
             </div>
